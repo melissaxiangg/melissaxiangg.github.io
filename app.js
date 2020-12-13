b372a86d776d538e1845f1818b3d30ff00b5b95f
Jacob Park <soobinjppark@gmail.com>
2020-12-13 14:39:43 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+$("h1").delay(500).animate({"opacity": "1"}, 2000);
+$("hr").delay(1000).animate({"opacity": "1"}, 1000);
+$("h5").delay(1500).animate({"opacity": "1"}, 2000);
+
+$("#icon1").delay(2500).animate({"opacity": "1"}, 500);
+$("#icon2").delay(2800).animate({"opacity": "1"}, 500);
+$("#icon3").delay(3100).animate({"opacity": "1"}, 500);
+$("#icon4").delay(3400).animate({"opacity": "1"}, 500);
+
